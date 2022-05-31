2fe89f2cdfd00b8f3a099740c67f0f9f49f099a2
Rajeev Yadav <rajeevy396@gmail.com>
2022-05-31 22:52:41 +0545

    day4 completed

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,65 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
+let logEntries = [];
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc}  ${operator}  ${calcNumber}`;
+  outputResult(currentResult, calcDescription);
+}
+
+function writeToLog(
+  operationIdentifier,
+  prevResult,
+  operationNumber,
+  newNumber
+) {
+  const logEntry = {
+    operation: operationIdentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newNumber,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntry.operation);
+  console.log(logEntries);
+}
 
 function add() {
-    currentResult = currentResult+userInput.value;
-    outputResult(currentResult, '');
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, currentResult);
+  writeToLog("ADD", initialResult, enteredNumber, currentResult);
 }
+
+function sub() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, currentResult);
+  writeToLog("SUBTRACT", initialResult, enteredNumber, currentResult);
+}
+
+function mul() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, currentResult);
+  writeToLog("MULTIPLY", initialResult, enteredNumber, currentResult);
+}
+
+function div() {
+  const enteredNumber = getUserNumberInput();
+  initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, currentResult);
+  writeToLog("DIVIDE", initialResult, enteredNumber, currentResult);
+}
+
 addBtn.addEventListener('click', add);
-// let calculationDescription = `(${defaultResult} + 10) * 3 / 2 - 1`;
+subBtn.addEventListener('click', sub);
+mulBtn.addEventListener('click', mul);
+divBtn.addEventListener('click', div);
--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,5 +1,8 @@
-const userInput =  document.getElementById('input-number');
+const usrInput =  document.getElementById('input-number');
 const addBtn = document.getElementById('btn-add');
+const subBtn = document.getElementById('btn-subtract');
+const mulBtn = document.getElementById('btn-multiply');
+const divBtn = document.getElementById('btn-divide');
 
 const currentCalculationOutput = document.getElementById('current-calculation');
 const currentResultOutput = document.getElementById('current-result');
